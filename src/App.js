42383f630b25f14e067f131219efa0c9ca6b84c9
unknown <mbilalghz@gmail.com>
2019-12-02 12:44:45 +0700

    first inital

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome To Dashboard
+        </p>
+        <a
+          className="App-link"
+          href="https://msygo.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Synergo
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
